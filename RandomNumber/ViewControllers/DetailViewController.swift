783ccd43d5284a340e0cfe067fd18d9f1ff3412d
Salakhoff <salakhoff.ilfat@gmail.com>
2023-02-13 13:40:40 +0300

    setting Detail VC

--- RandomNumber/ViewControllers/DetailViewController.swift
+++ RandomNumber/ViewControllers/DetailViewController.swift
@@ -11,7 +11,7 @@ final class DetailViewController: UIViewController {
     
     let minTextField = NumberTextField(placeholder: "min number...")
     let maxTextField = NumberTextField(placeholder: "max number...")
-    
+    let saveButton = RandomButtonNumber(textTitle: "Save")
     
     let stackView: UIStackView = {
         let stackView = UIStackView()
@@ -39,6 +39,8 @@ final class DetailViewController: UIViewController {
         stackView.addArrangedSubview(minTextField)
         stackView.addArrangedSubview(maxTextField)
         view.addSubview(stackView)
+        
+        view.addSubview(saveButton)
     }
     
     private func setupConstraints() {
@@ -46,6 +48,10 @@ final class DetailViewController: UIViewController {
             stackView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
             stackView.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 20),
             stackView.trailingAnchor.constraint(equalTo: view.trailingAnchor, constant: -20),
+            
+            saveButton.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            saveButton.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor, constant: -20),
+            saveButton.widthAnchor.constraint(equalTo: view.widthAnchor, multiplier: 0.7)
         ])
     }
     
